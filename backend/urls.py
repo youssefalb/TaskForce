49e8c73e42fda57b84d7831ad89fa4d68af2a3f3
youssefalb <yousalb891@student.polsl.pl>
2023-07-17 00:28:53 +0200

    setup the project + auth

--- backend/urls.py
+++ backend/urls.py
@@ -1,4 +1,4 @@
-"""TaskForceDjango URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from users.views import LoginView, UserRegistrationView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view()),
+    path('register/', UserRegistrationView.as_view(), name='user-registration'),
 ]
