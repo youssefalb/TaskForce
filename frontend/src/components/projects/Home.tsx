6c1573d7a736c9e90e09819426333efe718227a3
youssefalb <yousalb891@student.polsl.pl>
2023-10-27 18:55:38 +0200

    Now the task status can be updated in the db by drag and drop finally

--- frontend/src/components/projects/Home.tsx
+++ frontend/src/components/projects/Home.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Grid, Paper, Typography, Card, CardContent, Avatar } from '@mui/material';
+import { Container, Grid, Paper, Typography, Card, CardContent, Avatar, Button } from '@mui/material';
 import { Draggable, Droppable, DragDropContext } from 'react-beautiful-dnd';
 import { useSession } from 'next-auth/react';
-import { getProjectTasks } from '@/lib/projects';
+import { changeTaskStatus, getProjectTasks } from '@/lib/projects';
 
 const Home = ({ projectId }: any) => {
   type Task = {
@@ -10,12 +10,12 @@ const Home = ({ projectId }: any) => {
     title: string;
     status: string;
     description: string;
-    users : {
+    users: {
       id: string;
       username: string;
       image: string;
     }[];
-
+    deadline: string;
   };
 
   const [tasks, setTasks] = useState<Task[]>([]);
@@ -26,9 +26,17 @@ const Home = ({ projectId }: any) => {
       if (session?.user?.accessToken) {
         try {
           const data = await getProjectTasks(session.user.accessToken, projectId);
-          // Assign a unique ID to each task
-          const tasksWithUniqueIds = data.map((task, index) => ({ ...task, id: `task-${index}` }));
-          setTasks(tasksWithUniqueIds);
+          console.log('Data', data);
+
+
+          //Tasks but with string ids since the draggable component requires string ids
+        const tasksWithStringsIds = data.map((task: Task) => ({
+          ...task,
+          id: task.id.toString(),
+        }));
+
+
+          setTasks(tasksWithStringsIds);
           console.log('Response Data', data);
         } catch (error) {
           console.error(error);
@@ -40,10 +48,16 @@ const Home = ({ projectId }: any) => {
     fetchData();
   }, [session, projectId]);
 
-  const onDragEnd = (result: any) => {
+  const addTask = () => {
+    console.log('add task');
+  }
+
+  
+  const onDragEnd = async (result: any) => {
     if (!result.destination) {
       return;
     }
+
 
     const updatedTasks = Array.from(tasks);
     const [draggedTask] = updatedTasks.splice(result.source.index, 1);
@@ -52,72 +66,93 @@ const Home = ({ projectId }: any) => {
       status: result.destination.droppableId,
     });
 
-    setTasks(updatedTasks);
+    console.log('draggedTask: ', draggedTask);
+    console.log('Tasks: ', tasks);
+    await changeTaskStatus(session?.user?.accessToken, draggedTask.id, result.destination.droppableId)
+      .then((data) => {
+
+        
+        setTasks(updatedTasks);
+      })
+      .catch((error) => {
+        console.error('Error changing task status:', error);
+      });
   };
 
   return (
-    <Container>
-      <DragDropContext onDragEnd={onDragEnd}>
-        <Typography variant="h4" component="h1" align="center" gutterBottom>
-          Task Management
-        </Typography>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Typography variant="h4" component="h1" align="center" gutterBottom>
+        Task Management
+      </Typography>
 
-        <Grid container spacing={0.5} style={{ display: 'flex'}}>
-          {['backlog', 'todo', 'doing', 'done', 'scrapped'].map((columnId) => (
-            <Droppable key={columnId} droppableId={columnId} type="task">
-              {(provided) => (
-                <Grid item xs={2.4} ref={provided.innerRef}>
-                  <Paper className="p-4">
-                    <Typography variant="h6" className="text-lg font-semibold mb-4">
-                      {columnId}
-                    </Typography>
-                    {tasks.map((task, index) => {
-                      if (task.status === columnId) {
-                        return (
-                          <Draggable key={task.id} draggableId={task.id} index={index}>
-                            {(provided) => (
-                              <Card
-                                ref={provided.innerRef}
-                                {...provided.draggableProps}
-                                {...provided.dragHandleProps}
-                                elevation={3}
-                                className={`p-2 mb-2 ${
-                                  columnId === 'todo' ? 'bg-blue-100' :
-                                  columnId === 'doing' ? 'bg-yellow-100' :
+      <Grid container spacing={0.5} style={{ display: 'flex' }}>
+        {['backlog', 'todo', 'doing', 'done', 'scrapped'].map((columnId) => (
+          <Droppable key={columnId} droppableId={columnId} type="task">
+            {(provided) => (
+              <Grid item xs={2.4} ref={provided.innerRef}>
+                <Paper className="p-4">
+                  <div className="flex justify-between items-center mb-4">
+                  <Typography variant="h6" className="text-lg font-semibold mb-4">
+                    {columnId}
+                  </Typography>
+
+                  {columnId === 'backlog' && (
+                    <button onClick={addTask} className="p-2 m-2 text-black font-bold bg-zinc-300 rounded-2xl">
+                      + Add Task
+                    </button>)}
+                    </div>
+                  {tasks.map((task, index) => {
+                    if (task.status === columnId) {
+                      return (
+                        <Draggable key={task.id} draggableId={task.id} index={index}>
+                          {(provided) => (
+                            <Card
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              elevation={3}
+                              className={`p-2 mb-2 ${columnId === 'todo' ? 'bg-blue-100' :
+                                columnId === 'doing' ? 'bg-yellow-100' :
                                   columnId === 'scrapped' ? 'bg-red-100' : 'bg-green-100'
                                 } rounded`}
-                              >
-                                <CardContent>
-                                  <Typography variant="h6" component="div">
-                                    {task.title}
-                                  </Typography>
-                                  <Typography variant="body2" color="text.secondary">
+                            >
+                              <CardContent>
+                                <Typography variant="h6" component="div">
+                                  {task.title}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                  <div className='overflow-hidden  text-ellipsis'>
                                     {task.description}
-                                  </Typography>
-                                  <div>
-                                      {task.users.map((user) => (
-                                      <div key={user.id}>
-                                        <Avatar src={user.image} alt={user.username}  title={user.username}/>
-                                      </div>
-                                    ))}
                                   </div>
-                                </CardContent>
-                              </Card>
-                            )}
-                          </Draggable>
-                        );
-                      }
-                      return null;
-                    })}
-                    {provided.placeholder}
-                  </Paper>
-                </Grid>
-              )}
-            </Droppable>
-          ))}
-        </Grid>
-      </DragDropContext>
-    </Container>
+                                </Typography>
+                                <div>
+                                  {task.users.map((user) => (
+                                    <div key={user.id}>
+                                      <Avatar src={user.image} alt={user.username} title={user.username} />
+                                    </div>
+                                  ))}
+
+                                  <Typography variant="body2" align="right">
+                                    Due {task.deadline}
+                                  </Typography>
+                                </div>
+                              </CardContent>
+                            </Card>
+                          )}
+                        </Draggable>
+                      );
+                    }
+                    return null;
+                  })}
+                  {provided.placeholder}
+                </Paper>
+              </Grid>
+            )}
+          </Droppable>
+        ))}
+      </Grid>
+    </DragDropContext>
+    
   );
 };
 
